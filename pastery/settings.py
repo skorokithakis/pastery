aad8defe3b4938117fb7452449f77064ef700b1f
Stavros Korokithakis <stavros@korokithakis.net>
2015-12-05 21:04:34 +0200

    Add local settings.

--- pastery/settings.py
+++ pastery/settings.py
@@ -164,3 +164,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, "_static")
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
