cd964355fdebd8892d46318b62c8adc0c403bf53
Stelios Petrakis <stelios@evilwindowdog.com>
2015-12-05 21:58:25 +0200

    Share functionality

--- static/js/script.js
+++ static/js/script.js
@@ -22,7 +22,74 @@ var LanguageSelector = (function() {
   }
 })();
 
+var ShareSelector = (function() {
+
+  var init = function() {
+
+    if($('#share-dropdown').length == 0)
+      return;
+
+    $('[data-placement="tooltip"]').tooltip()
+
+    var label = $('#share-dropdown .dropdown-toggle em');
+    var choices = $('#share-dropdown .dropdown-menu a');
+
+    $(choices).on('click', function() {
+
+      if($(this).hasClass('selected'))
+        return;
+
+      $('#share-dropdown a.selected').removeClass('selected');
+      $(this).addClass('selected');
+      $(label).html($(this).data('choice'));
+
+    });
+
+    var input = $('#share-dropdown input');
+
+    $('#copy-clipboard').on("click", function(e) {
+
+      var element = $('#share-dropdown input')[0];
+
+      element.setSelectionRange(0, element.value.length);
+
+      try {  
+        document.execCommand('copy');  
+      } catch(err) { }
+
+      $(input).tooltip('show');
+
+      setTimeout(function() {
+
+        $(input).tooltip('hide');
+
+      }, 1000);
+    });
+
+    $(input).on("click", function(e) {
+
+      this.setSelectionRange(0, this.value.length);
+
+      // console.log(this);
+      // var range = document.createRange();
+      // range.selectNode(this);
+      // window.getSelection().addRange(range);
+
+      // try {  
+      //   document.execCommand('copy');  
+      // } catch(err) { }  
+
+      // $(e.target).one('mouseup', function(e) { e.preventDefault(); });
+    });
+  }
+
+  return {
+    'initialize': init
+  }
+})();
+
 $(document).ready(function() {
 
+  ShareSelector.initialize();
   LanguageSelector.initialize();
 });
