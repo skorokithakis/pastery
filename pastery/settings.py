1488df8c8824834e511faa9476e5f5db0ed91ba0
Stavros Korokithakis <stavros@korokithakis.net>
2015-12-06 00:49:43 +0200

    Add debug audiences.

--- pastery/settings.py
+++ pastery/settings.py
@@ -24,10 +24,9 @@ SECRET_KEY = '#60=4-b_z(wv06&gdr%zk5+-%r=590zl+x=j4_t1!*a-%&$r&n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+BROWSERID_AUDIENCES = []
 
 ALLOWED_HOSTS = [".pastery.net"]
-
-BROWSERID_AUDIENCES = ["https://www.pastery.net"]
 
 # Application definition
 
