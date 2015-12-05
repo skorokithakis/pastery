7fb5a54fe506efb016c98f6eaf4be9143f1b976c
Stavros Korokithakis <stavros@korokithakis.net>
2015-12-05 20:19:10 +0200

    Rename project.

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pastebin.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pastery.settings")
 
     from django.core.management import execute_from_command_line
 
