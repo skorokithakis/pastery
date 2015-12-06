bbfb2f27305e49cd38e07293e790faeb1dcf92dc
Stavros Korokithakis <stavros@korokithakis.net>
2015-12-06 05:21:59 +0200

    Reduce URL length.

--- main/urls.py
+++ main/urls.py
@@ -5,6 +5,6 @@ from main import views
 
 urlpatterns = [
     url(r'^$', views.home, name="home"),
-    url(r'^(?P<paste_id>[a-z]{4,})/raw/$', views.raw_paste, name="raw-paste"),
-    url(r'^(?P<paste_id>[a-z]{4,})/$', views.paste, name="paste"),
+    url(r'^(?P<paste_id>[a-z]{3,})/raw/$', views.raw_paste, name="raw-paste"),
+    url(r'^(?P<paste_id>[a-z]{3,})/$', views.paste, name="paste"),
 ]
