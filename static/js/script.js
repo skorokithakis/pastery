40665e52b23e58e2e5277d70d43a45349c594dc0
Stelios Petrakis <stelios@evilwindowdog.com>
2015-12-06 10:01:31 +0200

    fix

--- static/js/script.js
+++ static/js/script.js
@@ -12,7 +12,7 @@ var ShareSelector = (function() {
       var element = $(this).find('.btn-group input')[0];
 
       element.setSelectionRange(0, element.value.length);
-      
+
     });
 
     $('.action-copy button').on("click", function(e) {
@@ -49,7 +49,31 @@ var ShareSelector = (function() {
   }
 })();
 
+var FixWidth = (function() {
+
+  var init = function() {
+
+    if($('.pretty-paste').length == 0)
+      return;
+
+    var windowWidth = document.documentElement.clientWidth;
+    var containerWidth = $('.pretty-paste').width();
+    var codeWidth = $('.pretty-paste table').width();
+
+    if(windowWidth < codeWidth)
+      return;
+
+    $('.pretty-paste').addClass('enable');
+
+  };
+
+  return {
+    'initialize': init
+  }
+})();
+
 $(document).ready(function() {
 
+  FixWidth.initialize();
   ShareSelector.initialize();
 });
