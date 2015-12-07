7424c979b6fc6aed4f124f65c270b7586876171e
Stavros Korokithakis <stavros@korokithakis.net>
2015-12-07 19:44:00 +0200

    Improve API.

--- api/views.py
+++ api/views.py
@@ -1,8 +1,10 @@
 import datetime
+import json
 from collections import namedtuple
 
 from annoying.decorators import ajax_request
 from django.contrib.sites.shortcuts import get_current_site
+from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
 from django.utils import timezone
@@ -31,13 +33,25 @@ def paste(request):
     try:
         data = schema.validate(dict(request.GET))
     except SchemaError as e:
-        return {"result": "error", "error_msg": str(e)}
+        response = {"result": "error", "error_msg": str(e)}
+        return HttpResponse(json.dumps(response), content_type="application/json", status=422)
+
+    try:
+        body = request.body.decode("utf8")
+    except UnicodeDecodeError:
+        response = {"result": "error", "error_msg": "Your request body was not valid UTF-8."}
+        return HttpResponse(json.dumps(response), content_type="application/json", status=422)
 
     paste = Paste.objects.create(
             title=data.title,
             raw_language=data.language,
-            body=request.body,
+            body=body,
             expiration=timezone.now() + datetime.timedelta(minutes=data.duration),
             )
 
-    return {"url": "https://%s%s" % (get_current_site(request).domain, paste.get_absolute_url())}
+    return {
+        "url": "https://%s%s" % (get_current_site(request).domain, paste.get_absolute_url()),
+        "language": paste.language,
+        "title": paste.title,
+        "duration": data.duration,
+    }
