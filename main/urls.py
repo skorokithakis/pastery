33f26857c0466ee724e5c3befc36ae5cd1b61c0b
Stavros Korokithakis <stavros@korokithakis.net>
2015-12-07 10:35:50 +0200

    Make URLs case-insensitive more.

--- main/urls.py
+++ main/urls.py
@@ -6,7 +6,7 @@ from main import views
 urlpatterns = [
     url(r'^$', views.home, name="home"),
     url(r'^account/$', views.account, name="account"),
-    url(r'^(?P<paste_id>[a-z]{3,})/delete/$', views.delete_paste, name="delete-paste"),
-    url(r'^(?P<paste_id>[a-z]{3,})/raw/$', views.raw_paste, name="raw-paste"),
-    url(r'^(?P<paste_id>[a-z]{3,})/$', views.paste, name="paste"),
+    url(r'^(?P<paste_id>[^/]{3,})/delete/$', views.delete_paste, name="delete-paste"),
+    url(r'^(?P<paste_id>[^/]{3,})/raw/$', views.raw_paste, name="raw-paste"),
+    url(r'^(?P<paste_id>[^/]{3,})/$', views.paste, name="paste"),
 ]
