02a3322fe67747c98568117592ed7a96c6f7e46d
Stavros Korokithakis <stavros@korokithakis.net>
2015-12-08 20:12:24 +0200

    Fix username search in admin.

--- main/admin.py
+++ main/admin.py
@@ -6,7 +6,7 @@ from .models import Paste, User
 @admin.register(Paste)
 class SettingAdmin(admin.ModelAdmin):
     list_display = ["id", "user", "title", "created", "expiration", "has_expired"]
-    search_fields = ["user", "title"]
+    search_fields = ["user__username", "title"]
     list_filter = ('created', "expiration")
     ordering = ["-created"]
 
