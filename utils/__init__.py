0d1ce7d5f5c74b5d0a64227744827725768d3d52
Stavros Korokithakis <stavros@korokithakis.net>
2015-12-10 03:44:25 +0200

    Refactor Mixpanel addition.

--- utils/__init__.py
+++ utils/__init__.py
@@ -2,18 +2,24 @@ from mixpanel import Mixpanel
 from mixpanel_async import AsyncBufferedConsumer
 from django.conf import settings
 
-mp = Mixpanel(settings.MIXPANEL_TOKEN, consumer=AsyncBufferedConsumer())
+if getattr(settings, "MIXPANEL_TOKEN", None):
+    mp = Mixpanel(settings.MIXPANEL_TOKEN, consumer=AsyncBufferedConsumer())
+else:
+    mp = None
 
 
 def send_event(user_id, name, data):
     """Send an event to Mixpanel."""
-    if getattr(settings, "MIXPANEL_TOKEN", None) is None:
+    if not mp:
         return
 
     mp.track(user_id, name, data)
 
 
 def identify_user(user_id, user):
+    if not mp:
+        return
+
     mp.people_set(user_id, {
         "$email": user.email,
         "style": user._style_name
