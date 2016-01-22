4010c7c3475f27ce5e7f77d2a0c621fbd09edefa
Stavros Korokithakis <stavros@korokithakis.net>
2016-01-22 17:02:05 +0200

    Show page generation time.

--- pastery/settings.py
+++ pastery/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE_CLASSES = [
+    'pastery.stats_middleware.StatsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
