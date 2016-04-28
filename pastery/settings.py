d533c959b8e4462e0b86b2c5b17e240716f4e10b
Stavros Korokithakis <stavros@korokithakis.net>
2016-04-28 15:23:54 +0300

    chore: Add docker files.

--- pastery/settings.py
+++ pastery/settings.py
@@ -117,8 +117,12 @@ SITE_ID = 1
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': "postgres",  # Or path to database file if using sqlite3.
+        'USER': 'postgres',                      # Not used with sqlite3.
+        'PASS': 'password',                      # Not used with sqlite3.
+        'HOST': "db",                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': 5432,                      # Set to empty string for default. Not used with sqlite3.
     }
 }
 
