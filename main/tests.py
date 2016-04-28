325a328b2a34ba4a9454eeb78578b6d6caf856ca
Stavros Korokithakis <stavros@korokithakis.net>
2016-04-28 16:32:28 +0300

    chore: Improve tests.

--- main/tests.py
+++ main/tests.py
@@ -1,7 +1,8 @@
-from django.test import TestCase
+from django.test import TestCase, override_settings
 from django.core.urlresolvers import reverse
 
 
+@override_settings(PASSWORD_HASHERS=['django.contrib.auth.hashers.MD5PasswordHasher'])
 class SmokeTests(TestCase):
     def test_urls(self):
         response = self.client.get(reverse("home"))
