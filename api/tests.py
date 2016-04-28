da5aa4e80d61dc0fc3e0bf4872b1383aefe6fd3f
Stavros Korokithakis <stavros@korokithakis.net>
2016-04-28 19:05:44 +0300

    Fix URLs.

--- api/tests.py
+++ api/tests.py
@@ -7,3 +7,8 @@ class SmokeTests(TestCase):
     def test_urls(self):
         response = self.client.post(reverse("api:paste"), "stuff", content_type='application/x-www-form-urlencoded')
         self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json()["language"], "text")
+
+        paste_id = response.json()["id"]
+        response = self.client.get(reverse("api:paste-id", args=[paste_id]))
+        self.assertEqual(response.status_code, 422)
--- main/tests.py
+++ main/tests.py
@@ -5,6 +5,38 @@ from django.core.urlresolvers import reverse
 @override_settings(PASSWORD_HASHERS=['django.contrib.auth.hashers.MD5PasswordHasher'])
 class SmokeTests(TestCase):
     def test_urls(self):
-        print(reverse("main:home"))
         response = self.client.get(reverse("main:home"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_paste(self):
+        response = self.client.get(reverse("main:home"))
+        form = response.context["form"]
+        data = form.initial
+        data["body"] = "hello!"
+
+        response = self.client.post(reverse("main:home"), data, follow=True)
+        paste_id = response.context["paste"].id
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b"hello!", response.content)
+
+        response = self.client.get(reverse("main:paste", args=[paste_id]))
+        self.assertEqual(response.status_code, 200)
+
+        response = self.client.post(reverse("main:reset-key"), follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertRedirects(response, reverse("main:home"))
+
+        response = self.client.get(reverse("main:embed-paste", args=[paste_id]))
+        self.assertEqual(response.status_code, 200)
+
+        response = self.client.post(reverse("main:delete-paste", args=[paste_id]), follow=True)
+        self.assertEqual(response.status_code, 200)
+
+        response = self.client.post(reverse("main:report-paste", args=[paste_id]), follow=True)
+        self.assertEqual(response.status_code, 200)
+
+        response = self.client.post(reverse("main:raw-paste", args=[paste_id]))
+        self.assertEqual(response.status_code, 200)
+
+        response = self.client.post(reverse("main:download-paste", args=[paste_id]))
         self.assertEqual(response.status_code, 200)
