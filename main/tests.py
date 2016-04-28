d72a3f6bb7f9e3fc37ff24b786b160576c0b8b62
Stavros Korokithakis <stavros@korokithakis.net>
2016-04-28 16:27:27 +0300

    chore: Change deployment database and add tests.

--- main/tests.py
+++ main/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
+from django.core.urlresolvers import reverse
 
-# Create your tests here.
+
+class SmokeTests(TestCase):
+    def test_urls(self):
+        response = self.client.get(reverse("home"))
+        self.assertEqual(response.status_code, 200)
--- pastery/settings.py
+++ pastery/settings.py
@@ -115,16 +115,25 @@ SITE_ID = 1
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': "postgres",  # Or path to database file if using sqlite3.
-        'USER': 'postgres',                      # Not used with sqlite3.
-        'PASS': 'password',                      # Not used with sqlite3.
-        'HOST': "db",                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': 5432,                      # Set to empty string for default. Not used with sqlite3.
+if os.environ.get("IN_DOCKER"):
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+            'NAME': "postgres",  # Or path to database file if using sqlite3.
+            'USER': 'postgres',                      # Not used with sqlite3.
+            'PASS': 'password',                      # Not used with sqlite3.
+            'HOST': "db",                      # Set to empty string for localhost. Not used with sqlite3.
+            'PORT': 5432,                      # Set to empty string for default. Not used with sqlite3.
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
