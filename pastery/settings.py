b95944215411f9b3782e96696b23df8a65ff737a
Stavros Korokithakis <stavros@korokithakis.net>
2016-04-29 19:29:51 +0300

    Fix password parameter.

--- pastery/settings.py
+++ pastery/settings.py
@@ -121,7 +121,7 @@ if os.environ.get("IN_DOCKER"):
             'ENGINE': 'django.db.backends.postgresql_psycopg2',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
             'NAME': "postgres",  # Or path to database file if using sqlite3.
             'USER': 'postgres',                      # Not used with sqlite3.
-            'PASS': 'password',                      # Not used with sqlite3.
+            'PASSWORD': 'password',                      # Not used with sqlite3.
             'HOST': "db",                      # Set to empty string for localhost. Not used with sqlite3.
             'PORT': 5432,                      # Set to empty string for default. Not used with sqlite3.
         }
