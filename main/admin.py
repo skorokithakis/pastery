b5d15c35d2e9336792127e08bc59a17cbf371306
Stavros Korokithakis <stavros@korokithakis.net>
2017-01-09 15:18:09 +0200

    Add "purge user" action.

--- main/admin.py
+++ main/admin.py
@@ -5,11 +5,26 @@ from .models import Paste, User
 
 
 @admin.register(Paste)
-class SettingAdmin(admin.ModelAdmin):
+class PasteAdmin(admin.ModelAdmin):
     list_display = ["id", "user", "title", "created", "expiration", "views", "max_views", "has_expired"]
     search_fields = ["user__username", "title"]
     list_filter = ('created', "expiration")
     ordering = ["-created"]
+    actions = ["purge_user"]
+
+    def purge_user(self, request, queryset):
+        for paste in queryset:
+            user_counter = 0
+            paste_counter = 0
+            if paste.user:
+                paste_counter += paste.user.paste_set.count()
+                paste.user.delete()
+                user_counter += 1
+            else:
+                paste.delete()
+                paste_counter += 1
+            self.message_user(request, "%s users and %s pastes deleted." % (user_counter, paste_counter))
+    purge_user.short_description = "Delete selected pastes and their users"
 
 
 @admin.register(User)
