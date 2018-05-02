73779367b8f109094dc7c685e6f7d5dad88721ac
Stavros Korokithakis <hi@stavros.io>
2018-05-02 19:55:23 +0300

    Actually set an upper bound on the duration of a paste via the API

--- api/views.py
+++ api/views.py
@@ -73,7 +73,7 @@ class PasteView(View):
             And({
                 Optional("title", default=""): And([str], Use(lambda x: x[0]), lambda x: len(x) < 200, error="\"title\" should be a string less than 200 characters long."),
                 Optional("language", default="autodetect"): And([str], Use(lambda x: x[0]), Use(lambda x: ALIAS_DICT.get(x, "autodetect")), error="\"language\" should be the name of a supported language."),
-                Optional("duration", default=1440): And([str], Use(lambda x: int(x[0])), lambda x: 100 * 365 * 24 * 60 * x > 0, error="\"duration\" should be an integer number of minutes before the paste is deleted."),
+                Optional("duration", default=1440): And([str], Use(lambda x: int(x[0])), lambda x: x <= 100 * 365 * 24 * 60, error="\"duration\" should be an integer number of minutes before the paste is deleted."),
                 Optional("api_key", default=None): Use(lambda x: User.objects.get(api_key=x[0]), error="\"api_key\" must be a valid API key."),
                 Optional("max_views", default=0): And([str], Use(lambda x: int(x[0])), lambda x: x >= 0, error="\"max_views\" should be a non-negative integer number of views before the paste is deleted."),
                 },
