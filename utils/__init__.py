56336dda72f9dc2465c24f93e89504aee41ce98b
Stavros Korokithakis <hi@stavros.io>
2023-05-10 15:02:35 +0300

    Remove Mixpanel

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,30 +1,7 @@
-from django.conf import settings
-from mixpanel import Mixpanel
-from mixpanel_async import AsyncBufferedConsumer
-
-if getattr(settings, "MIXPANEL_TOKEN", None):
-    mp = Mixpanel(settings.MIXPANEL_TOKEN, consumer=AsyncBufferedConsumer())
-else:
-    mp = None
-
-
 def send_event(user_id, name, data):
     """Send an event to Mixpanel."""
-    if not mp:
-        return
-
-    mp.track(user_id, name, data)
+    return
 
 
 def identify_user(user):
-    if not mp:
-        return
-
-    if isinstance(user.username, bytes):
-        # It may sometimes occur that persona usernames are bytes before being
-        # saved. We guard against that here.
-        username = user.username.decode("utf8")
-    else:
-        username = user.username
-
-    mp.people_set(username, {"$email": user.email, "style": user._style_name})
+    return
